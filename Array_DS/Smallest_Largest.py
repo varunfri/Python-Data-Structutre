136bd0509fc0bf795ccf206dda69e738844efa6b
Varun K V <varunvishwanath1@gmail.com>
2025-02-03 14:13:05 +0530

    Create Smallest_Largest.py
    Using the numpy.min(array) and numpy.max(array)

---
+++ Array_DS/Smallest_Largest.py
@@ -0,0 +1,10 @@
+# Create a function to find the min and max values of array
+
+def min_max_fun(arr_np):
+    min = np.min(arr_np)
+    max = np.max(arr_np)
+
+    return min, max
+
+print(min_max_fun(arr_1)
+)
