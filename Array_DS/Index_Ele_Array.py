743aba653465a30f280ad638e124a0478fe498f9
Varun K V <varunvishwanath1@gmail.com>
2025-02-03 15:03:57 +0530

    Create Index_Ele_Array.py

---
+++ Array_DS/Index_Ele_Array.py
@@ -0,0 +1,9 @@
+# Index of ele in array
+def index_array_ele(arr_x, ele):
+     return arr_2.index(ele)
+    
+arr_2 = arr.array('i',[1,2,2,3,4,5,65,66])
+
+ele = int(input())
+
+print(f"Index of {ele} (len: 0-{len(arr_2)}) is: : {arr_2.index(ele)}")
