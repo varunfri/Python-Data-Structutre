206c91222ec46486e5e71c360dc963c5de4fabf9
Varun K V <varunvishwanath1@gmail.com>
2025-02-03 14:08:24 +0530

    Created array_create.py

---
+++ Array_DS/array_create.py
@@ -0,0 +1,9 @@
+# Create an array using numpy and array library 
+import array as arr
+import numpy as np
+
+arr_1 = arr.array([1,2,3,4,5])
+arr_np = np.array([11,12,13,14,15])
+
+print("Array using the Array library:",arr_1)
+print("Created an array using numpy: ",arr_np)
