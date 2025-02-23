73e9bcea5314e6fea5ebc327b2a630a299e2ca24
Varun K V <varunvishwanath1@gmail.com>
2025-02-23 21:01:57 +0530

    Update Q01.py

--- Array_DS/practice_question_array/Intermediate/Q01.py
+++ Array_DS/practice_question_array/Intermediate/Q01.py
@@ -1,1 +1,18 @@
+# Move all Zeros to end 
+def move_zero(arr):
 
+    n = len(arr)
+    j = 0
+
+    for i in range(n):
+        print(i, j)
+        if arr[i] != 0:
+            arr[j], arr[i] = arr[i], arr[j]
+            j += 1
+            print(arr)
+            
+
+    return arr
+
+ar = [0,1,0,3,0]
+print("After Moving all zero to end: ", move_zero(ar))
